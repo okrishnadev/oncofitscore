6ec67847c4e77bb8a74c71f566b8334133dbbaf0
Krishna Dev Oruganty <okdev@sriramlab1.engin.umich.edu>
2018-02-19 13:58:43 -0500

    Notebook added

---
+++ push.sh
@@ -0,0 +1,6 @@
+echo "# oncofitscore" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/okrishnadev/oncofitscore.git
+git push -u origin master
